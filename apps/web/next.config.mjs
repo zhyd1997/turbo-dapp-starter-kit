6075d8ab12ea59c71857a89b434d17f642d94b11
Yadong Zhang <zhyd007@gmail.com>
2025-06-05 18:52:17 +0800

    fix(apps/web): add `pino-pretty` to webpack `externals` configuration

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 }
 
 export default nextConfig
