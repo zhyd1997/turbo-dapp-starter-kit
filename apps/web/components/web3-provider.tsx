68563a6e3e002c6cd7eed47e979559c1f4cdd812
Yadong Zhang <zhyd007@gmail.com>
2025-06-06 09:22:13 +0800

    refactor: wagmi and web3 provider configurations

--- apps/web/components/web3-provider.tsx
+++ apps/web/components/web3-provider.tsx
@@ -1,25 +1,25 @@
-"use client"
+"use client";
 
-import {
-    RainbowKitProvider,
-  } from '@rainbow-me/rainbowkit';
-  import { WagmiProvider } from 'wagmi';
-  import {
-    QueryClientProvider,
-    QueryClient,
-  } from "@tanstack/react-query";
-import { config } from '@/lib/wagmi';
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { config } from "@/lib/wagmi";
 
-  const client = new QueryClient();
+const client = new QueryClient();
 
 export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <WagmiProvider config={config}>
+  return (
+    <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-            {children}
+        <RainbowKitProvider
+          appInfo={{
+            appName: "Turbo DApp Starter Kit",
+            learnMoreUrl: "https://git.new/turbo-dapp-starter-kit",
+          }}
+        >
+          {children}
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
-    )
-}
+  );
+};
--- apps/web/lib/wagmi.ts
+++ apps/web/lib/wagmi.ts
@@ -1,18 +1,21 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import type { Config } from 'wagmi';
-import {
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    bsc,
-    bscTestnet,
-  } from 'wagmi/chains';
+import { createConfig, http, injected } from "wagmi";
+import type { Config } from "wagmi";
+import { hardhat, mainnet } from "wagmi/chains";
+import { createClient } from "viem";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+
+export const hardHatConfig = createConfig({
+  chains: [hardhat],
+  connectors: [injected()],
+  client({ chain }) {
+    return createClient({ chain, transport: http("http://127.0.0.1:8545") });
+  },
+  ssr: true,
+});
 
 export const config: Config = getDefaultConfig({
-    chains: [mainnet, polygon, optimism, arbitrum, base, bsc, bscTestnet],
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '',
-    appName: 'Turbo DApp Starter Kit',
-    ssr: true,
+  chains: [mainnet],
+  appName: "Turbo DApp Starter Kit",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  ssr: true,
 });
