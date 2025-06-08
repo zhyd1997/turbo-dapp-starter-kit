8d551c7c4b95be2094fcfb5447bc26dd7dc09535
Yadong Zhang <zhyd007@gmail.com>
2025-06-08 22:14:07 +0800

    fix: `compile` contract library before build web app.

--- apps/web/hooks/contracts/useLock.ts
+++ apps/web/hooks/contracts/useLock.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useReadContract } from "wagmi";
-// import { abi as lockAbi } from "@workspace/contracts/artifacts/contracts/Lock.sol/Lock.json";
+import { abi as lockAbi } from "@workspace/contracts/artifacts/contracts/Lock.sol/Lock.json";
 // import deployedAddresses from "@workspace/contracts/ignition/deployments/chain-31337/deployed_addresses.json"
 import { hardhat } from "wagmi/chains";
 
@@ -12,7 +12,7 @@ export const useLock = () => {
   const readContractResult = useReadContract<any, any, any, any, any>({
     // FIXME: UNCOMMENT IT TO MAKE IT WORK
     // address: deployedAddresses['LockModule#Lock'] as `0x${string}`,
-    // abi: lockAbi,
+    abi: lockAbi,
     functionName: "unlockTime",
     chainId: hardhat.id,
   });
